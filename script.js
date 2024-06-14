31666e7db155a757035e5ec50682ad0404f1424e
MartinaBisi98 <martina.bisi@outlook.com>
2024-06-14 17:20:59 +0200

    created repo

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const filterButtons = document.querySelectorAll('.filter-button');
+    const cardItems = document.querySelectorAll('.card-item');
+  
+    filterButtons.forEach(function (button) {
+      button.addEventListener('click', function () {
+        const filter = button.getAttribute('data-filter');
+        
+        cardItems.forEach(function (item) {
+          if (filter === 'all' || item.getAttribute('data-tags').includes(filter)) {
+            item.style.display = 'block';
+          } else {
+            item.style.display = 'none';
+          }
+        });
+    });
+});
+});
