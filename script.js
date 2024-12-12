6860b12b944f10a148c53ed04850303386c6979e
BisiMartina <bisimartina1@gmail.com>
2024-12-12 17:16:28 +0100

    Edited, Scroll top down added

--- script.js
+++ script.js
@@ -19,3 +19,31 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 
+// Scroll To Top Button
+
+document.addEventListener('DOMContentLoaded', function () {
+  // Bottone "Scroll To Top"
+  const mybutton = document.querySelector(".topbtn");
+
+  window.onscroll = function () {
+    scrollFunction();
+  };
+
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+  }
+
+  function topFunction() {
+    console.log("topFunction eseguita");
+    document.body.scrollTop = 0; // Safari
+    document.documentElement.scrollTop = 0; // Chrome, Firefox, IE, Opera
+  }
+
+  // Associa la funzione al bottone (può essere utile per debug)
+  mybutton.addEventListener("click", topFunction);
+});
+
